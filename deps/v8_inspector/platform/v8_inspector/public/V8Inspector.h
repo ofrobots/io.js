41d73c477ecf41337b2fa4071a1571cf97d27882
Ali Ijaz Sheikh <ofrobots@google.com>
2016-05-10 12:28:33 -0700

    deps: import v8_inspector
    Pick up v8 inspector from [1]. This is the standalone version of the
    devtools debug protocol. Submodules of v8_inspector: jinja2, wtf, and
    markupsafe are also included. jinja2 and markupsafe are compile-time
    dependencies. We expect that the dependency on wtf will be replaced in
    the future with STL.
    
    [1] https://github.com/repenaxa/v8_inspector/commit/3171115

---
+++ deps/v8_inspector/platform/v8_inspector/public/V8Inspector.h
@@ -0,0 +1,69 @@
+// Copyright 2015 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef V8Inspector_h
+#define V8Inspector_h
+
+#include "platform/v8_inspector/public/V8DebuggerClient.h"
+#include "platform/v8_inspector/public/V8InspectorSession.h"
+
+#include "wtf/OwnPtr.h"
+#include <v8.h>
+
+namespace blink {
+
+namespace protocol {
+class Dispatcher;
+class Frontend;
+class FrontendChannel;
+}
+
+class V8Debugger;
+class V8DebuggerAgent;
+class V8HeapProfilerAgent;
+class V8ProfilerAgent;
+
+class V8Inspector : public V8DebuggerClient {
+    WTF_MAKE_NONCOPYABLE(V8Inspector);
+public:
+    explicit V8Inspector(v8::Isolate*, v8::Local<v8::Context>, v8::Platform*);
+
+    // V8DebuggerClient overrides.
+    void runMessageLoopOnPause(int contextGroupId) override;
+    void quitMessageLoopOnPause() override;
+
+    void eventListeners(v8::Local<v8::Value>, V8EventListenerInfoList&) override;
+    bool callingContextCanAccessContext(v8::Local<v8::Context> calling, v8::Local<v8::Context> target) override;
+    String16 valueSubtype(v8::Local<v8::Value>) override;
+    bool formatAccessorsAsProperties(v8::Local<v8::Value>) override;
+    void muteWarningsAndDeprecations() override { }
+    void unmuteWarningsAndDeprecations() override { }
+    double currentTimeMS() override { return 0; };
+
+    void muteConsole() override;
+    void unmuteConsole() override;
+    bool isExecutionAllowed() override;
+    int ensureDefaultContextInGroup(int contextGroupId) override;
+
+    ~V8Inspector();
+
+    // Transport interface.
+    void connectFrontend(protocol::FrontendChannel*);
+    void disconnectFrontend();
+    void dispatchMessageFromFrontend(const String16& message);
+
+private:
+    v8::Isolate* m_isolate;
+    v8::Platform* m_platform;
+    v8::Global<v8::Context> m_context;
+    bool m_terminated;
+    OwnPtr<V8Debugger> m_debugger;
+    OwnPtr<V8InspectorSession> m_session;
+    OwnPtr<protocol::Dispatcher> m_dispatcher;
+    OwnPtr<protocol::Frontend> m_frontend;
+};
+
+}
+
+#endif // V8Inspector_h
