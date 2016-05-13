5c640acf1616a1f9bbebbd4e15b2c120ea230e83
Pavel Feldman <pfeldman@chromium.org>
2016-05-13 11:29:14 -0700

    Allow detaching and re-attaching to the debugger, brush up code.

--- src/inspector_agent.h
+++ src/inspector_agent.h
@@ -39,62 +39,52 @@ class Agent {
   // Stop the inspector agent
   void Stop();
 
-  void PostMessages();
-
   bool IsStarted();
   bool connected() {  return connected_; }
 
  protected:
-  inline node::Environment* parent_env() {  return parent_env_; }
-  void InitAdaptor(Environment* env);
+  static void ThreadCbIO(Agent* agent);
+  static void OnSocketConnectionIO(uv_stream_t* server, int status);
+  static bool OnInspectorHandshakeIO(inspector_socket_t* socket,
+                                     enum inspector_handshake_event state,
+                                     const char* path);
+  static void OnRemoteDataIO(uv_stream_t* stream, ssize_t read,
+      const uv_buf_t* b);
+  static void WriteCbIO(uv_async_t* async);
 
-  // Worker body
-  void WorkerRun();
+  void WorkerRunIO();
+  void OnInspectorConnectionIO(inspector_socket_t* socket);
 
-  static void ThreadCb(Agent* agent);
-  static void ParentSignalCb(uv_async_t* signal);
+  void PostMessages();
+  void SetConnected(bool connected);
+  void Write(const blink::protocol::String16& message);
 
   uv_sem_t start_sem_;
   uv_cond_t pause_cond_;
   uv_mutex_t queue_lock_;
   uv_mutex_t pause_lock_;
-  std::vector<blink::protocol::String16> message_queue_;
+  uv_thread_t thread_;
+  uv_loop_t child_loop_;
+  uv_tcp_t server_;
 
   int port_;
   bool wait_;
   bool connected_;
-
-  uv_thread_t thread_;
   node::Environment* parent_env_;
-  uv_loop_t child_loop_;
 
-  uv_tcp_t server_;
-  uv_async_t dataWritten_;
-  // Currently it is simply the last inspector connection. Later we may consider
-  // some sort of policy - e.g. closing a previous connection or supporting
-  // multiple connections.
+  uv_async_t data_written_;
   inspector_socket_t* client_socket_;
   blink::V8Inspector* inspector_;
   v8::Platform* platform_;
+  std::vector<blink::protocol::String16> message_queue_;
   bool dispatching_messages_;
-
-  static bool OnInspectorHandshake(inspector_socket_t* socket,
-                                   enum inspector_handshake_event state,
-                                   const char* path);
-  static void OnSocketConnection(uv_stream_t* server, int status);
-  static void OnRemoteData(uv_stream_t* stream, ssize_t read,
-      const uv_buf_t* b);
-  static void WriteCb(uv_async_t* async);
-
-  bool RespondToGet(inspector_socket_t* socket, const char* path);
-  bool AcceptsConnection(inspector_socket_t* socket, const char* path);
-  void OnInspectorConnection(inspector_socket_t* socket);
-  void Write(const blink::protocol::String16& message);
 
   friend class AsyncWriteRequest;
   friend class ChannelImpl;
+  friend class DispatchOnInspectorBackendTask;
   friend class SetConnectedTask;
   friend class V8NodeInspector;
+  friend void InterruptCallback(v8::Isolate*, void* agent);
 };
 
 }  // namespace inspector
